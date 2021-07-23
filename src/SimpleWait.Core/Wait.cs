fcda2642aafcf5af5e5ef23a6edf93941a8b0c6a
taja01 <69lamasd@gmail.com>
2021-07-23 06:43:17 +0200

    implement wait based on selenium default wait without selenium dependency

---
+++ src/SimpleWait.Core/Wait.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace SimpleWait.Core
+{
+    public class Wait
+    {
+        private readonly IWait<bool> wait;
+        private static readonly Type DefaultException = typeof(TimeoutException);
+        private Type exceptionType = DefaultException;
+        public Wait()
+        {
+            this.wait = new DefaultWait<bool>(true) { Timeout = TimeSpan.FromSeconds(5) };
+        }
+
+        public static Wait Initialize()
+        {
+            return new Wait();
+        }
+
+        public Wait IgnoreExceptionTypes(params Type[] exceptionTypes)
+        {
+            this.wait.IgnoreExceptionTypes(exceptionTypes);
+            return this;
+        }
+
+        public Wait Throw<T>() where T : Exception
+        {
+            this.exceptionType = typeof(T);
+            return this;
+        }
+
+        public Wait Timeout(TimeSpan? timeout)
+        {
+            if (timeout.HasValue)
+            {
+                this.wait.Timeout = timeout.Value;
+            }
+
+            return this;
+        }
+
+        public Wait Message(string message)
+        {
+            this.wait.Message = message;
+            return this;
+        }
+
+        public Wait PollingInterval(TimeSpan pollingInterval)
+        {
+            this.wait.PollingInterval = pollingInterval;
+            return this;
+        }
+
+        public bool Success(Func<bool> condition)
+        {
+            try
+            {
+                this.Until(condition);
+                return true;
+            }
+            catch (TimeoutException)
+            {
+                return false;
+            }
+        }
+
+        public void Until(Func<bool> condition)
+        {
+            bool Func(bool b) => condition();
+            try
+            {
+                _ = this.wait.Until(Func);
+            }
+            catch (TimeoutException e) when (this.exceptionType != DefaultException)
+            {
+                throw (Exception)Activator.CreateInstance(this.exceptionType, e.Message);
+            }
+        }
+
+        public void Until<T>(Func<T> func, Func<T, bool> success, Func<T, string> message)
+        {
+            T t = default;
+            try
+            {
+                _ = this.wait.Until(_ =>
+                {
+                    t = func();
+                    return success(t);
+                });
+            }
+            catch (TimeoutException e) when (this.exceptionType == DefaultException)
+            {
+                throw (Exception)Activator.CreateInstance(this.exceptionType, $"{e.Message} | {message(t)}");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public TResult Until<TResult>(Func<TResult> condition)
+        {
+            TResult Func(bool b) => condition();
+            try
+            {
+                return this.wait.Until(Func);
+            }
+            catch (TimeoutException e) when (this.exceptionType != DefaultException)
+            {
+                throw (Exception)Activator.CreateInstance(this.exceptionType, e.Message);
+            }
+        }
+    }
+}
---
+++ test/SimpleWait.CoreTest/CoreTests.cs
@@ -0,0 +1,123 @@
+using NUnit.Framework;
+using SimpleWait.Core;
+using System;
+using System.Diagnostics;
+
+namespace SimpleWait.CoreTest
+{
+    public class Tests
+    {
+        [Test]
+        public void DefaultTimeoutTest()
+        {
+            Assert.Throws<TimeoutException>(() => Wait.Initialize().Message("Default timeout test").Until(() => false));
+        }
+
+        [Test]
+        public void TimeoutTest()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            Assert.Throws<TimeoutException>(() => Wait.Initialize().Timeout(TimeSpan.FromSeconds(1)).Message("Timeout test").Until(() => false));
+            sw.Stop();
+            Assert.That(sw.ElapsedMilliseconds > 1000 && sw.ElapsedMilliseconds < 1300);
+        }
+
+        [Test]
+        public void ExceptionThrowTest()
+        {
+            Assert.Throws<DivideByZeroException>(() => Wait.Initialize().Timeout(TimeSpan.FromMilliseconds(1)).Throw<DivideByZeroException>().Message("Exception throw").Until(() => false));
+        }
+
+        [Test]
+        public void TestIgnoreExceptionTest()
+        {
+            var zero = 0;
+            var one = 1;
+
+            Assert.Throws<TimeoutException>(() =>
+            {
+                Wait.Initialize()
+                   .Message("Ignore Exception test")
+                   .Timeout(TimeSpan.FromSeconds(1))
+                   .IgnoreExceptionTypes(typeof(DivideByZeroException))
+                   .Until(() => one / zero == 0);
+            });
+        }
+
+        [Test]
+        public void SuccessTimeoutTest()
+        {
+            var m_array = new[] { 1, 3, 5, 7, 9, 10 };
+
+            var result = Wait.Initialize()
+                   .Message("Success test")
+                   .Success(() =>
+                   {
+                       return false;
+                   });
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void SuccessTest()
+        {
+            var m_array = new[] { 1, 3, 5, 7, 9, 10 };
+            var index = 0;
+            var result = Wait.Initialize()
+                   .Message("Success test")
+                   .Success(() =>
+                   {
+                       if (m_array[index] % 2 == 0)
+                       {
+                           return true;
+                       }
+                       else
+                       {
+                           index++;
+                           return false;
+                       }
+                   });
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void PollingTest()
+        {
+            var counter = -1;
+
+            Wait.Initialize()
+                .PollingInterval(TimeSpan.FromSeconds(3))
+                .Success(() =>
+                {
+                    counter++;
+                    return false;
+                });
+
+            Assert.AreEqual(2, counter);
+        }
+
+        [Test]
+        public void TypedUntilTest()
+        {
+            var r = new WorkingClass();
+
+            var index = 0;
+            var result = Wait.Initialize()
+                .Until(() =>
+                {
+                    return new WorkingClass();
+                });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(typeof(WorkingClass), result.GetType());
+        }
+    }
+
+    class WorkingClass
+    {
+
+    }
+}
